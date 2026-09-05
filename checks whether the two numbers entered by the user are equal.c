831957db89ecba94c680ef9cd2ecfc8c950a034f
RITIKA SRIVASTAVA <ritikasrivastava0408@gmail.com>
2026-09-05 11:09:20 +0530

    Add files via upload

---
+++ checks whether the two numbers entered by the user are equal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    if(a==b)
+        printf("Both numbers are equal");
+    else
+        printf("Both numbers are unequal");
+    return 0;
+}
